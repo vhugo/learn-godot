0cd806be0dd4ba979e9ca2d1c308419e11f4f67c
Victor Alves <victor@sub.pro.br>
2018-02-16 06:08:04 +0100

    Add missing signal connection to HUD

--- 03_first_game/hud.gd
+++ 03_first_game/hud.gd
@@ -5,11 +5,11 @@ signal start_game
 func show_message(text):
 	$MessageLabel.text = text
 	$MessageLabel.show()
-	$MessageLabel.start()
+	$MessageTimer.start()
 
 func show_game_over():
 	show_message("Game Over")
-	yield($MessageLabel, "timeout")
+	yield($MessageTimer, "timeout")
 	$StartButton.show()
 	$MessageLabel.text = "Dodge the\nCreeps!"
 	$MessageLabel.show()
@@ -17,4 +17,9 @@ func show_game_over():
 func update_score(score):
 	$ScoreLabel.text = str(score)
 
+func _on_StartButton_pressed():
+    $StartButton.hide()
+    emit_signal("start_game")
 
+func _on_MessageTimer_timeout():
+    $MessageLabel.hide()
